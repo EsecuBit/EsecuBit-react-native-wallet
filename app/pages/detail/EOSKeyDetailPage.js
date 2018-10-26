184accf87467708775d85648646636d2c3238bb4
Sekfung <sekfung.lau@gmail.com>
2018-10-26 15:35:29 +0800

    Add Copy Function For EOSKeyDetailPage

--- app/pages/detail/EOSKeyDetailPage.js
+++ app/pages/detail/EOSKeyDetailPage.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text } from 'react-native'
-import { View, Container, Textarea } from 'native-base'
+import { StyleSheet, Text, Clipboard } from 'react-native'
+import { View, Container, Textarea, Icon } from 'native-base'
 import BaseToolbar from '../../components/BaseToolbar'
 import { Color, Dimen } from '../../common/Styles'
+import ToastUtil from '../../utils/ToastUtil'
 
 export default class EOSKeyDetailPage extends Component {
 
   constructor() {
     super()
+    this.state = {
+      ownerKey: 'testOwnerKey',
+      activeKey: 'testActiveKey'
+    }
+  }
+
+  _setClipboardContent(key) {
+    try {
+      Clipboard.setString(key)
+      ToastUtil.showShort('Copy Success')
+    } catch (error) {
+      ToastUtil.showShort('Copy Failed')
+    }
   }
   
   render() {
     return (
       <Container>
         <BaseToolbar title='EOS Key' />
-        <View padder>
-          <Text style={styles.keyTitle}>Owner Key</Text>
-          <Textarea style={{fontSize: Dimen.PRIMARY_TEXT}} rowSpan={3} bordered disabled value='testtesttesttesttest' />
-          <Text style={[styles.keyTitle, {marginTop: Dimen.SPACE}]}>Active Key</Text>
-          <Textarea style={{fontSize: Dimen.PRIMARY_TEXT}} rowSpan={3} bordered disabled value='testtesttesttesttest'  />
+        <View style={{padding: Dimen.MARGIN_HORIZONTAL}}>
+          <View style={{flexDirection: 'row', alignItems: 'stretch'}}>
+            <Text style={styles.keyTitle}>Owner Key</Text>
+            <View style={{flex: 1, alignItems: 'flex-end'}}>
+              <Icon name='copy' style={{color: Color.DISABLE_BG}} onPress={() => this._setClipboardContent(this.state.ownerKey)}/>
+            </View>
+          </View>
+          <Textarea style={{fontSize: Dimen.PRIMARY_TEXT}} rowSpan={3} bordered disabled value={this.state.ownerKey} />
+          <View style={{flexDirection: 'row', alignItems: 'stretch', marginTop: Dimen.MARGIN_VERTICAL}}>
+            <Text style={styles.keyTitle}>Active Key</Text>
+            <View style={{flex: 1, alignItems: 'flex-end'}}>
+              <Icon name='copy' style={{color: Color.DISABLE_BG}} onPress={() => this._setClipboardContent(this.state.activeKey)}/>
+            </View>
+          </View>
+          <Textarea style={{fontSize: Dimen.PRIMARY_TEXT}} rowSpan={3} bordered disabled value={this.state.activeKey}  />
           <Text style={styles.tips}>{'Tips: \n' + 'testtesttest'}</Text>
         </View>
       </Container>
@@ -28,6 +52,7 @@ export default class EOSKeyDetailPage extends Component {
 
 const styles = StyleSheet.create({
   keyTitle: {
+    flex: 1,
     color: Color.ACCENT,
     fontSize: Dimen.PRIMARY_TEXT
   },
