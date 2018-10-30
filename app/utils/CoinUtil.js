d7ee7dbfc37d5f4572beb41db55b005ffcca80fb
Sekfung <sekfung.lau@gmail.com>
2018-10-30 09:52:21 +0800

    Update CoinUtil

--- app/utils/CoinUtil.js
+++ app/utils/CoinUtil.js
@@ -2,78 +2,53 @@ import { EsWallet, D } from 'esecubit-wallet-sdk'
 import PreferenceUtil from '../utils/PreferenceUtil'
 import { LEGAL_CURRENCY_UNIT_KEY } from '../common/Constants'
 
-var CoinUtil = (function() {
-  var instance
-  function CoinUtil() {
+class CoinUtil {
+
+  static async minimumCryptoCurrencyToLegalCurrency(coinType, value) {
     let wallet = new EsWallet()
-
-    return {
-      //minimum crypto currency unit to legal currency unit displayed
-      minimumCryptoCurrencyToLegalCurrency: async function(coinType, value) {
-        let fromUnit = D.isBtc(coinType) ? D.unit.btc.satoshi : D.unit.eth.Wei
-        let legalCurrencyUnit = await PreferenceUtil.getCurrencyUnit(
-          LEGAL_CURRENCY_UNIT_KEY
-        )
-        return Number(
-          wallet.convertValue(coinType, value, fromUnit, legalCurrencyUnit)
-        ).toFixed(2)
-      },
-      minimumCryptoCurrencyToDefautCurrency: async function(coinType, value) {
-        let fromUnit = D.isBtc(coinType) ? D.unit.btc.satoshi : D.unit.eth.Wei
-        let toUnit = await PreferenceUtil.getCryptoCurrencyUnit(coinType)
-        return wallet.convertValue(coinType, value, fromUnit, toUnit)
-      },
-      //crypto currency unit displayed to legal currency unit displayed
-      defaultCryptoCurrencyToLegalCurrency: async function(coinType, value) {
-        let fromUnit = await PreferenceUtil.getCryptoCurrencyUnit(coinType)
-        let legalCurrencyUnit = await PreferenceUtil.getCurrencyUnit(
-          LEGAL_CURRENCY_UNIT_KEY
-        )
-        return Number(
-          wallet.convertValue(coinType, value, fromUnit, legalCurrencyUnit)
-        ).toFixed(2)
-      },
-      //legal currency unit displayed to crypto currency unit displayed
-      defaultLegalCurrencyToCryptoCurrency: async function(coinType, value) {
-        let toUnit = await PreferenceUtil.getCryptoCurrencyUnit(coinType)
-        let legalCurrencyUnit = await PreferenceUtil.getCurrencyUnit(
-          LEGAL_CURRENCY_UNIT_KEY
-        )
-        return wallet.convertValue(coinType, value, legalCurrencyUnit, toUnit)
-      },
-      //legal currency unit displayed to minimum cryto currency unit
-      defaultLegalCurrencyToMinimumCryptoCurrency: async function(
-        coinType,
-        value
-      ) {
-        let toUnit = D.isBtc(coinType) ? D.unit.btc.satoshi : D.unit.eth.Wei
-        let legalCurrencyUnit = await PreferenceUtil.getCurrencyUnit(
-          LEGAL_CURRENCY_UNIT_KEY
-        )
-        return wallet.convertValue(coinType, value, legalCurrencyUnit, toUnit)
-      },
-      //crypto currency unit displayed to minimum cryto currency unit
-      defaultCryptoCurrencyToMinimumCryptoCurrency: async function(
-        coinType,
-        value
-      ) {
-        let fromUnit = await PreferenceUtil.getCryptoCurrencyUnit(coinType)
-        let toUnit = D.isBtc(coinType) ? D.unit.btc.satoshi : D.unit.eth.Wei
-        return wallet.convertValue(coinType, value, fromUnit, toUnit)
-      },
-      getExchangeRate: async function(coinType, value) {
-        return this.defaultCryptoCurrencyToLegalCurrency(coinType, value)
-      }
-    }
+    let fromUnit = D.isBtc(coinType) ? D.unit.btc.satoshi : D.unit.eth.Wei
+    let legalCurrencyUnit = await PreferenceUtil.getCurrencyUnit(LEGAL_CURRENCY_UNIT_KEY)
+    return Number(wallet.convertValue(coinType, value, fromUnit, legalCurrencyUnit)).toFixed(2)
   }
 
-  return {
-    getInstance: function() {
-      if (!instance) {
-        instance = new CoinUtil()
-      }
-      return instance
-    }
+  static async minimumCryptoCurrencyToDefautCurrency(coinType, value) {
+    let wallet = new EsWallet()
+    let fromUnit = D.isBtc(coinType) ? D.unit.btc.satoshi : D.unit.eth.Wei
+    let toUnit = await PreferenceUtil.getCryptoCurrencyUnit(coinType)
+    return wallet.convertValue(coinType, value, fromUnit, toUnit)
   }
-})()
+
+  static async defaultCryptoCurrencyToLegalCurrency(coinType, value) {
+    let wallet = new EsWallet()
+    let fromUnit = await PreferenceUtil.getCryptoCurrencyUnit(coinType)
+    let legalCurrencyUnit = await PreferenceUtil.getCurrencyUnit(LEGAL_CURRENCY_UNIT_KEY)
+    return Number(wallet.convertValue(coinType, value, fromUnit, legalCurrencyUnit)).toFixed(2)
+  }
+
+  static async defaultLegalCurrencyToCryptoCurrency(coinType, value) {
+    let wallet = new EsWallet()
+    let toUnit = await PreferenceUtil.getCryptoCurrencyUnit(coinType)
+    let legalCurrencyUnit = await PreferenceUtil.getCurrencyUnit(LEGAL_CURRENCY_UNIT_KEY)
+    return wallet.convertValue(coinType, value, legalCurrencyUnit, toUnit)
+  }
+
+  static async defaultLegalCurrencyToMinimumCryptoCurrency(coinType, value) {
+    let wallet = new EsWallet()
+    let toUnit = D.isBtc(coinType) ? D.unit.btc.satoshi : D.unit.eth.Wei
+    let legalCurrencyUnit = await PreferenceUtil.getCurrencyUnit(LEGAL_CURRENCY_UNIT_KEY)
+    return wallet.convertValue(coinType, value, legalCurrencyUnit, toUnit)
+  }
+
+  static async defaultCryptoCurrencyToMinimumCryptoCurrency(coinType, value) {
+    let wallet = new EsWallet()
+    let fromUnit = await PreferenceUtil.getCryptoCurrencyUnit(coinType)
+    let toUnit = D.isBtc(coinType) ? D.unit.btc.satoshi : D.unit.eth.Wei
+    return wallet.convertValue(coinType, value, fromUnit, toUnit)
+  }
+
+  static async getExchangeRate(coinType, value) {
+    return this.defaultCryptoCurrencyToLegalCurrency(coinType, value)
+  }
+}
+
 export default CoinUtil
