40ae2b5ad125e5c98a7e3780bd5187c23bb54379
sekfung <sekfung.lau@gmail.com>
2018-12-22 14:17:26 +0800

    Add transition

--- app/router/Router.js
+++ app/router/Router.js
@@ -1,12 +1,25 @@
 import RouterConfig from "./RouterConfig"
-import { StackNavigator }from 'react-navigation'
+import { StackNavigator } from "react-navigation"
+import CardStackStyleInterpolator from "react-navigation/src/views/CardStack/CardStackStyleInterpolator"
+import { Easing, Animated } from "react-native"
 
 const EsecuBitNavigator = StackNavigator(RouterConfig, {
   navigationOptions: {
     header: null
   },
-  initialRouteName: 'Handler',
-  swipeEnabled: false,
-  animationEnabled: false
+  transitionConfig: () => {
+    return {
+      screenInterpolator: CardStackStyleInterpolator.forHorizontal,
+      transitionSpec: {
+        duration: 550,
+        easing: Easing.out(Easing.poly(4)),
+        timing: Animated.timing,
+        useNativeDriver: true
+      }
+    }
+  },
+  initialRouteName: "Handler",
+  swipeEnabled: true,
+  animationEnabled: true
 })
 export default EsecuBitNavigator
