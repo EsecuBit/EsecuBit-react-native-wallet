cc385016ebc0db3f4f82dacbc79276278df8761f
sekfung <sekfung.lau@gmail.com>
2020-07-02 18:09:03 +0800

    add i18n RNProvider config

--- app/Config.js
+++ app/Config.js
@@ -2,6 +2,7 @@ import {D, Provider, RealmDB, BtTransmitter, CryptoNative} from 'esecubit-react-
 import Routers from './router/Router'
 import RouterConfig from "./router/RouterConfig";
 import RNProvider from "esecubit-react-native-wallet-sdk/RNProvider";
+import I18n  from './lang/i18n'
 
 
 
@@ -16,10 +17,17 @@ export default {
     D.test.jsWallet = false
     D.network.type = 'auto'
     console.disableYellowBox = true
+    // 以下Provider配置项都是要配置
+    // 数据库
     Provider.DB = RealmDB
+    // Transmitter
     Provider.Transmitters.push(BtTransmitter)
     Provider.Crypto = CryptoNative
+    // App路由表
     RNProvider.Router.Config = RouterConfig
+    // 路由
     RNProvider.Router.Router = Routers
+    // 多语言
+    RNProvider.I18n = I18n
   }
 }
